c47c53f4a0dd0560081d5147124f6c7ac5778dd8
“Mineliegoma” <garlandegoma@gmail.com>
2020-11-08 21:24:11 -0500

    rendered the individual collection in the collection page

--- src/pages/collections/Collection.js
+++ src/pages/collections/Collection.js
@@ -1,48 +1,40 @@
 import React from 'react'
 import './collection.styles.scss'
-import _ from "lodash"
 import { connect } from 'react-redux'
-// import Preview from '../../components/preview-collection/Preview.js'
 
-// import { selectCollection } from '../../redux/shop/shop.selectors';
 import CollectionItem from '../../components/collection-item/CollectionItem'
-// import ApiOverview from '../../components/api-overview/ApiOverview.js'
+import './collection.styles.scss'
 
-const Collection = ({ collections }) => {
+const Collection = ({ collect }) => {
+
+ // const { details, title } = collect? 
+ console.log('hehehehe for the collect', collect)
 
 
- console.log('hehehehe', collections)
 
- // constrenderSpecificCollection = (obj) => {
- //  collections.find(collection => collection.id === collection(obj))
- // }
 
  return (
-  <div className='category'>
-   <h2>CATEGORY PAGE</h2>
-   {/* {this.renderItemsbyId()} */}
-   {/* <ApiOverview /> */}
+  <div className='collection-page'>
+   <h2 className='title'>{collect?.title}</h2>
+   <div className='items'>
+    {
+     collect?.details?.map(detail => (<CollectionItem key={detail.id} detail={detail} />))
+    }
+
+   </div>
 
 
   </div>
- )
+ );
 
 
 }
 
-// const mapStateToProps = () => ({
-//  // item: this.state.find(state, { id: ownProps.params.CollectionId })
-
-
-// });
-// const mapStateToProps = (state, ownProps) => ({ item: state.api.find((e) => e.id == ownProps.match.params.collectionId) });
-
-
 function mapStateToProps(state, ownProps) {
  console.log('hehe', ownProps)
  return {
-  collections: state.api.find((obj) => obj.pathName == ownProps.match.params.collectionId)
-  // 
+  collect: state.api.find((obj) => obj.pathName === ownProps.match.params.collectionId)
+
   // collections: state.api.find((obj) => e.pathName == ownProps.params.collectionId)
  }
 }
@@ -51,48 +43,3 @@ function mapStateToProps(state, ownProps) {
 export default connect(mapStateToProps)(Collection);
 
 
-// import React from 'react'
-// import { connect } from 'react-redux'
-// // import { createStructuredSelector } from 'reselect';
-// // import { selectCollections } from '../../redux/shop/shop.selectors'
-// import Preview from '../../components/preview-collection/Preview.js'
-// // import { getItems } from '../../redux/shop/shop.actions'
-// // import Search from '../../components/search/Search.js'
-// import './apiOverview.styles.scss'
-
-
-
-// class ApiOverview extends React.Component {
-
-//  //  renderItems = () => {
-//  //     return this.props.items.map(({ id, ...otherCollectionProps }) => (<Preview key={id} {...otherCollectionProps} />))
-//  //   } 
-
-//  render() {
-
-//   // console.log('hehe', this.props)
-//   return (
-//    <>
-//     {/* <Search search={this.state.search} searchHandler={this.searchChange} /> */}
-//     <div className="collections-overview">
-//      {
-//       this.props.items.map(({ id, ...otherCollectionProps }) => (<Preview key={id} {...otherCollectionProps} />))
-
-//      }
-
-//      {/* {this.renderItems()} */}
-//     </div>
-//    </>
-
-//   )
-//  }
-// };
-// function mapStateToProps(state) {
-//  // console.log('hehe', state.api)
-//  return {
-//   items: state.api
-//  }
-
-// }
-
-// export default connect(mapStateToProps)(ApiOverview);
